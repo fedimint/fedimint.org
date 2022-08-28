ceceadd65cff4afc59685ec268e6912e557720aa
elsirion <elsirion@protonmail.com>
2022-08-28 16:40:07 +0200

    s/Fedimints/Fedimint/

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Learn more about Fedimints
+            Learn more about Fedimint
           </Link>
         </div>
       </div>
